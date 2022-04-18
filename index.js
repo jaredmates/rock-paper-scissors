a911c3986b6aa8d79b7d6055be38d0172dfe23a5
Jared Mates <jaredmates45@gmail.com>
2022-04-17 17:34:35 -0700

    implemented function for a single round of the game

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-let choices = ["Rock", "Paper", "Scissors"];
-
-function computerPlay(arr) {
-  let randomChoice = Math.floor(Math.random() * arr.length);
-  return arr[randomChoice];
+function computerPlay() {
+  let choices = ["Rock", "Paper", "Scissors"];
+  let randomChoice = Math.floor(Math.random() * choices.length);
+  return choices[randomChoice];
 }
 
-console.log(computerPlay(choices));
-console.log(computerPlay(choices));
-console.log(computerPlay(choices));
-console.log(computerPlay(choices));
-console.log(computerPlay(choices));
-console.log(computerPlay(choices));
+function oneRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a Tie!";
+  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You Win! Scissors beats Paper";
+  } else {
+    return "Invalid input. Try again.";
+  }
+}
+
+console.log(oneRound("Rock", computerPlay()));
