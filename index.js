e744e6bd3cee2a637829d26c85c2b30402f73749
Jared Mates <jaredmates45@gmail.com>
2022-04-23 15:57:03 -0700

    set up a loop for the button event listeners and made a function for updating the score

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-// const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button");
 const playerScore = document.querySelector(".player-score");
 const computerScore = document.querySelector(".computer-score");
-
 const roundResult = document.querySelector(".round-result");
-
-const rockButton = document.querySelector(".rock-btn");
-const paperButton = document.querySelector(".paper-btn");
-const scissorsButton = document.querySelector(".scissors-btn");
 
 let playerNum = 0;
 let computerNum = 0;
-
-function computerPlay() {
-  let choices = ["Rock", "Paper", "Scissors"];
-  let randomChoice = Math.floor(Math.random() * choices.length);
-  console.log(choices[randomChoice]);
-  return choices[randomChoice];
-}
 
 // Creates Reset Button that appears after 5 rounds
 const content = document.createElement("button");
@@ -26,77 +14,61 @@ content.addEventListener("click", () => {
   location.reload();
 });
 
-rockButton.addEventListener("click", () => {
-  let computerChoice = computerPlay();
-  if (playerNum < 5 && computerNum < 5) {
-    if (rockButton.textContent === computerChoice) {
-      roundResult.textContent = "Its a Tie!";
-    } else if (computerChoice === "Paper") {
-      roundResult.textContent = "You Lose! Paper beats Rock.";
-      computerNum++;
-      computerScore.textContent = computerNum.toString();
-    } else if (computerChoice === "Scissors") {
-      roundResult.textContent = "You Win! Rock beats Scissors.";
-      playerNum++;
-      playerScore.textContent = playerNum.toString();
-    }
-  } else {
-    if (playerNum > computerNum) {
-      roundResult.textContent = "You Win!";
-      roundResult.append(content);
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let computerChoice = computerPlay();
+    let selection = button.textContent;
+    if (playerNum < 5 && computerNum < 5) {
+      playRound(selection, computerChoice);
     } else {
-      roundResult.textContent = "You Lose!";
-      roundResult.append(content);
+      if (playerNum > computerNum) {
+        roundResult.textContent = "You Win!";
+        roundResult.append(content);
+      } else {
+        roundResult.textContent = "You Lose!";
+        roundResult.append(content);
+      }
     }
-  }
+  });
 });
 
-paperButton.addEventListener("click", () => {
-  let computerChoice = computerPlay();
-  if (playerNum < 5 && computerNum < 5) {
-    if (paperButton.textContent === computerChoice) {
-      roundResult.textContent = "Its a Tie!";
-    } else if (computerChoice === "Rock") {
-      roundResult.textContent = "You Win! Paper beats Rock.";
-      playerNum++;
-      playerScore.textContent = playerNum.toString();
-    } else if (computerChoice === "Scissors") {
-      roundResult.textContent = "You Lose! Scissors beats Paper.";
-      computerNum++;
-      computerScore.textContent = computerNum.toString();
-    }
-  } else {
-    if (playerNum > computerNum) {
-      roundResult.textContent = "You Win!";
-      roundResult.append(content);
-    } else {
-      roundResult.textContent = "You Lose!";
-      roundResult.append(content);
-    }
-  }
-});
+function computerPlay() {
+  let choices = ["Rock", "Paper", "Scissors"];
+  let randomChoice = Math.floor(Math.random() * choices.length);
+  console.log(choices[randomChoice]);
+  return choices[randomChoice];
+}
 
-scissorsButton.addEventListener("click", () => {
-  let computerChoice = computerPlay();
-  if (playerNum < 5 && computerNum < 5) {
-    if (scissorsButton.textContent === computerChoice) {
-      roundResult.textContent = "Its a Tie!";
-    } else if (computerChoice === "Paper") {
-      roundResult.textContent = "You Win! Scissors beats Paper.";
-      playerNum++;
-      playerScore.textContent = playerNum.toString();
-    } else if (computerChoice === "Rock") {
-      roundResult.textContent = "You Lose! Rock beats Scissors.";
-      computerNum++;
-      computerScore.textContent = computerNum.toString();
-    }
-  } else {
-    if (playerNum > computerNum) {
-      roundResult.textContent = "You Win!";
-      roundResult.append(content);
-    } else {
-      roundResult.textContent = "You Lose!";
-      roundResult.append(content);
-    }
+function increaseComputerScore() {
+  computerNum++;
+  computerScore.textContent = computerNum.toString();
+}
+
+function increasePlayerScore() {
+  playerNum++;
+  playerScore.textContent = playerNum.toString();
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a Tie!";
+  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    roundResult.textContent = "You Lose! Paper beats Rock.";
+    increaseComputerScore();
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    roundResult.textContent = "You Win! Paper beats Rock.";
+    increasePlayerScore();
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    roundResult.textContent = "You Lose! Rock beats Scissors.";
+    increaseComputerScore();
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    roundResult.textContent = "You Win! Rock beats Scissors.";
+    increasePlayerScore();
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    roundResult.textContent = "You Lose! Scissors beats Paper.";
+    increaseComputerScore();
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    roundResult.textContent = "You Win! Scissors beats Paper.";
+    increasePlayerScore();
   }
-});
+}
